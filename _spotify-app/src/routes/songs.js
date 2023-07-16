92cfbd44c9a8f83b8a9c5659fa78e736d52c6e74
yovindoardana <yovindoardanaa@gmail.com>
2023-07-17 02:00:19 +0700

    rename SongController function

--- _spotify-app/src/routes/songs.js
+++ _spotify-app/src/routes/songs.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const SongController = require('../controllers/songs');
 
 // GET ALL SONGS
-router.get('/', SongController.getAllSongs);
+router.get('/', SongController.getAllSongsHandler);
 
 // ADD SONG
 router.post('/', SongController.addSongHandler);
 
 // GET SONG BY ID
-router.get('/:id', SongController.getSongById);
+router.get('/:id', SongController.getSongByIdHandler);
 
 // PLAY SONG
 router.get('/:id/play', SongController.playSongHandler);
